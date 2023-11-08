c0ced8e4657ce2b29a4c5ea471826f83ea56f7f5
Jay <138824448+jaythemself@users.noreply.github.com>
2023-11-07 23:42:04 -0600

    task 2.2 prep, task 2.2 incomplete with failing controller tests

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -45,3 +45,17 @@ public class EmployerController {
 
     }
 }
+
+// Two handlers with missing information. Task here is to make use of the EmployerRepository class in these handlers.
+// 1. Add private field EmployerRepository type called employerRepository to EmployerController. Give this field an @ Autowired annotation.
+// 2. Add index method that responds at /employers with a list of all employers in the database. This method should use the template
+// employers/index. To figure out the name of the model attribute you should use to pass employers into view, review this template.
+// 3. processAddEmployerForm already sends form back if any submitted employer object info is invalid. However, doesn't yet save a valid object.
+// Use employerRepository and the appropriate method to do so.
+// 4. displayViewEmployer will be in charge of rendering a page to view the contents of an indiv. employer object. Will make use of
+// that employer object's id field to grab correct info from employerRepo. optEmployer currently initialized to null. Replace this by
+// using the .findById() method w the right arg to look for the given employer object from the data layer. (variable holding id you want to query for is
+// already provided for you in the controller mehtod's parameters)
+// 5. Create a SkillController class and replicate steps above for EmployerController. New controller should have the methods, index, displayAddSkillForm,
+// processAddSkillForm, and displayViewSkill. These methods should behave the same. Relevant templates already created
+// 6. Test it with SQL (reference textbook and demo app again for this section)
