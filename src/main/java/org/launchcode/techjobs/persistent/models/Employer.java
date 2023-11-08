8beba4a71da71daf9a91274490bc806504086e0d
Jay <138824448+jaythemself@users.noreply.github.com>
2023-11-07 23:08:32 -0600

    task 2 incomplete, partially passing tests

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "This field must be filled out.")
+    @Size(min = 1, max = 100, message = "Must contain between 1-100 characters.")
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation() {
+        this.location = location;
+    }
+
 }
+
+// In addition to the fields inherited from AbstractEntity, Employer should have a string field for location. add the
+// field for location with validation that ensures it's not empty and has reasonable length
+// In addition, add public accessor methods (getter methods) to Employer. (test also looks for setter so. not just accessor method.)
+// Employer can have only one location.
+// Mapped to a table. @ Entity annotation, as well as no arg constructor
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size (max = 250, message = "Description limited to 250 characters.")
+    private String description;
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription() {
+        this.description = description;
+    }
+
 }
+
+// Add field for a longer description of skill, named description with public accessor methods
+// Give @ Entity annotation and be sure it contains no-arg constructor
